e660e798b15aed6c79c459ccf3fef7684c407ad8
Felipe <Felipe.simoesdarocha@gmail.com>
2023-03-17 22:54:32 -0300

    components + hooks
    tem que configurar o .env

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import InfoModal from '@/components/InfoModal';
+import useMovieList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
+import useInfoModalStore from '@/hooks/useInfoModalStore';
+
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
@@ -19,12 +27,19 @@ export async function getServerSideProps(context: NextPageContext) {
   }
 }
 
-
 const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModalStore();
+
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
       <div className="pb-40">
-        <h1>opa</h1>
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </>
   )
